e006f86dff5d7506d60f0e3e278cbdaf0c5254c8
Jordan Manus <jlmanus17@gmail.com>
2023-11-14 12:07:58 -0500

    added albums to albums class. updated index.html for albums app. working on getting css and JS connected to all html. having issues with albums/index.html using user/base.html for base template (states - album/base.html does not exist)

--- albums/models.py
+++ albums/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
+
 # Create your models here.
+class Album(models.Model):
+    album_name = models.CharField(max_length=500, blank=True)
+    artist = models.CharField(max_length=150, blank=True)
+    num_of_tracks = models.IntegerField(null=True, blank=True)
+    release_date = models.DateField()
+
+    def __str__(self) -> str:
+        return f'{self.album_name}'
+    
--- albums/views.py
+++ albums/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from .models import Album
+
 
 # Create your views here.
+def list_albums(request):
+    album = Album.objects.all()
+    return render(request, 'albums/index.html', {'album': album})
