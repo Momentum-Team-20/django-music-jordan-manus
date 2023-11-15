e96a82113e9e4639671d0a2cbe9815c0e9e7069e
Jordan Manus <jlmanus17@gmail.com>
2023-11-15 11:36:28 -0500

    added edit album function to views. updated create_album to post_album that will create and update album details. need to clean up albums templates folder, and add functionality to delete album function.

--- albums/views.py
+++ albums/views.py
@@ -1,7 +1,8 @@
+from django.contrib import messages
 from django.shortcuts import render, get_object_or_404, redirect
+from django.views import generic
 from .forms import AlbumForm
 from .models import Album
-from django.views import generic
 
 
 # Create your views here.
@@ -10,18 +11,37 @@ def home(request):
     return render(request, 'albums/index.html', {'albums': albums})
 
 
-def create_album(request, album_pk):
-    album = get_object_or_404(Album, pk=album_pk)
+def create_album(request):
     if request.method == 'POST':
         form = AlbumForm(request.POST)
         new_album = form.save(commit=False)
-        new_album.album = album
         new_album.save()
         return redirect('home')
     form = AlbumForm()
-    return render(request, 'create_album.html', {'form': form})
+    return render(request, 'albums/post_album.html', {'form': form})
 
 
 class AlbumDetailsView(generic.DetailView):
     model = Album
     template_name = 'albums/album_details.html'
+
+
+def edit_album(request, album_pk):
+    post = get_object_or_404(Album, pk=album_pk)
+
+    if request.method == 'GET':
+        context = {'form': AlbumForm(instance=post), 'pk': album_pk}
+        return render(request, 'albums/post_album.html', context)
+    elif request.method == 'POST':
+        form = AlbumForm(request.POST, instance=post)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'The post has been updated successfully.')
+            return redirect('home')
+        else:
+            messages.error(request, 'Please correct the follwoing errors: ')
+            return render(request, 'albums/post_form.html', {'form': form})
+
+
+def delete_album(request, album_pk):
+    pass
